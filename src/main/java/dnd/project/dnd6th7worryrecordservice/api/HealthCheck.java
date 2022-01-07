5912352d3f88b2dceb0c2611834b79ffcdc71577
jackson <steady8@kakao.com>
2022-01-08 01:20:37 +0900

    CI/CD: 무중단 배포 init

---
+++ src/main/java/dnd/project/dnd6th7worryrecordservice/api/HealthCheck.java
@@ -0,0 +1,12 @@
+package dnd.project.dnd6th7worryrecordservice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping("/check")
+    public String healthCheck() {
+        return "OK";
+    }
+}
