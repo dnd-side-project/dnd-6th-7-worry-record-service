4d8034743d5cae6c50095bb642485d7870471409
jackson <steady8@kakao.com>
2022-01-07 22:30:54 +0900

    git init

---
+++ src/main/java/dnd/project/dnd6th7worryrecordservice/Dnd6th7WorryRecordServiceApplication.java
@@ -0,0 +1,13 @@
+package dnd.project.dnd6th7worryrecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd6th7WorryRecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dnd6th7WorryRecordServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dnd/project/dnd6th7worryrecordservice/Dnd6th7WorryRecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.project.dnd6th7worryrecordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd6th7WorryRecordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
