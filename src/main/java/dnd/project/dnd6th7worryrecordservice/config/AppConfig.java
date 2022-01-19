db86e5228412a8ec55e9b7a2e3acf5ebe2cd8d57
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-01-19 23:44:23 +0900

    feat : kakao 로그인, jwt 검증 구현중 #4
    FRONT PART
    1. Front 에서 카카오에 로그인하여 '인가 코드' 흭득
    2. Front 에서 '인가 코드' 사용하여 'AccessToken' 흭득
    3. 서버로 'AccessToken' 전송
    
    BACK PART
    1. 'AccessToken' 사용하여 유저정보 받아오기 (완료)
    2. 유저정보 DB에 저장 (완료)
    3. 유저정보 기반으로 JWT AccessToken, RefreshToken 발급하여 return(완료)
    4. Refresh Token DB 저장 (미완) - USER_AUTH 테이블 만들지 USER 테이블에 저장할 지 토의해야함.
    5. 만료된 토큰 갱신 (미완)

--- src/main/java/dnd/project/dnd6th7worryrecordservice/config/AppConfig.java
+++ src/main/java/dnd/project/dnd6th7worryrecordservice/config/AppConfig.java
@@ -1,6 +1,7 @@
 package dnd.project.dnd6th7worryrecordservice.config;
 
 import dnd.project.dnd6th7worryrecordservice.domain.user.UserRepository;
+import dnd.project.dnd6th7worryrecordservice.kakao.KakaoService;
 import dnd.project.dnd6th7worryrecordservice.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,4 +16,7 @@ public class AppConfig {
 
     @Bean
     public UserServiceImpl userService() { return new UserServiceImpl();}
+
+    @Bean
+    public KakaoService kakaoService() { return new KakaoService(); }
 }
