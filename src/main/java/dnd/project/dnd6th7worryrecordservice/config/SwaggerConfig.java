57a7ea7a403d4bcc094211d472f4a6b1da9293ac
PgmJun <chltmdwns96@naver.com>
2022-03-15 14:15:49 +0900

    fix: SwaggerConfig 수정
    API_NAME, API_DESCRIPTION 변경

--- src/main/java/dnd/project/dnd6th7worryrecordservice/config/SwaggerConfig.java
+++ src/main/java/dnd/project/dnd6th7worryrecordservice/config/SwaggerConfig.java
@@ -17,9 +17,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
-    private static final String API_NAME = "WorryRecordService API";
+    private static final String API_NAME = "흐릿 API";
     private static final String API_VERSION = "0.0.1";
-    private static final String API_DESCRIPTION = "WorryRecordService API 명세서";
+    private static final String API_DESCRIPTION = "흐릿 API 명세서";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
