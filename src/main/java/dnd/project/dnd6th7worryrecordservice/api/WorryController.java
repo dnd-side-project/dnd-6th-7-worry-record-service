236d4b840cb29e8dee3f0c9f03282de5bc22cb8a
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-03-24 20:43:31 +0900

    bug: 시큐리티, 스웨거 활성화

--- src/main/java/dnd/project/dnd6th7worryrecordservice/api/WorryController.java
+++ src/main/java/dnd/project/dnd6th7worryrecordservice/api/WorryController.java
@@ -5,7 +5,7 @@ import dnd.project.dnd6th7worryrecordservice.dto.worry.request.WorryReviewModify
 import dnd.project.dnd6th7worryrecordservice.dto.worry.response.*;
 import dnd.project.dnd6th7worryrecordservice.service.WorryService;
 import io.jsonwebtoken.lang.Assert;
-//import io.swagger.annotations.*;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -22,10 +22,10 @@ public class WorryController {
     private final WorryService worryService;
 
     //홈 화면
-    /*@ApiOperation(value = "홈 화면", notes = "홈 화면에 필요한 데이터 호출")
+    @ApiOperation(value = "홈 화면", notes = "홈 화면에 필요한 데이터 호출")
     @ApiImplicitParam(
             name = "userId"
-            , value = "유저PK")*/
+            , value = "유저PK")
     @GetMapping("/home")
     public ResponseEntity<WorryHomeResponseDto> home(@RequestParam("userId") Long userId) {
         try {
@@ -37,7 +37,7 @@ public class WorryController {
     }
 
     //홈 - 걱정 작성
-//    @ApiOperation(value = "홈 - 걱정 작성", notes = "걱정 작성 후 저장")
+    @ApiOperation(value = "홈 - 걱정 작성", notes = "걱정 작성 후 저장")
     @PostMapping("/write")
     public ResponseEntity<WorryWriteResponseDto> addWorry(@RequestBody WorryRequestDto worryRequestDto) {
         try {
@@ -50,10 +50,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 요즘 걱정
-   /* @ApiOperation(value = "걱정 보관함 - 요즘 걱정", notes = "걱정 보관함에서 모든 요즘 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 요즘 걱정", notes = "걱정 보관함에서 모든 요즘 걱정 불러오기")
     @ApiImplicitParam(
             name = "userId"
-            , value = "유저PK")*/
+            , value = "유저PK")
     @GetMapping("/recent")
     public ResponseEntity<List<WorryResponseDto>> allRecentWorryList(@RequestParam("userId") Long userId) {
         try {
@@ -66,7 +66,7 @@ public class WorryController {
     }
 
     //걱정 보관함 - 요즘 걱정 - 걱정 카테고리 필터링
-    /*@ApiOperation(value = "걱정 보관함 - 요즘 걱정 카테고리 필터링", notes = "걱정 보관함에서 카테고리 필터링된 요즘 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 요즘 걱정 카테고리 필터링", notes = "걱정 보관함에서 카테고리 필터링된 요즘 걱정 불러오기")
     @ApiImplicitParams({
             @ApiImplicitParam(
                     name = "userId"
@@ -75,7 +75,7 @@ public class WorryController {
                     name = "categories"
                     , value = "카테고리PK List"
             )
-    })*/
+    })
     @GetMapping("/recent/filter")
     public ResponseEntity<List<WorryResponseDto>> categorizedRecentWorry(@RequestParam("userId") Long userId, @RequestParam("categories") List<Long> categoryId) {
         try {
@@ -88,10 +88,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 지난 걱정
-    /*@ApiOperation(value = "걱정 보관함 - 지난 걱정", notes = "걱정 보관함에서 모든 지난 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 지난 걱정", notes = "걱정 보관함에서 모든 지난 걱정 불러오기")
     @ApiImplicitParam(
             name = "userId"
-            , value = "유저PK")*/
+            , value = "유저PK")
     @GetMapping("/past")
     public ResponseEntity<List<WorryPastResponseDto>> allPastWorryList(@RequestParam("userId") Long userId) {
         try {
@@ -104,7 +104,7 @@ public class WorryController {
     }
 
     //걱정 보관함 - 지난 걱정 - 걱정 카테고리 필터링
-    /*@ApiOperation(value = "걱정 보관함 - 지난 걱정 카테고리 필터링", notes = "걱정 보관함에서 카테고리 필터링된 지난 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 지난 걱정 카테고리 필터링", notes = "걱정 보관함에서 카테고리 필터링된 지난 걱정 불러오기")
     @ApiImplicitParams({
             @ApiImplicitParam(
                     name = "userId"
@@ -113,7 +113,7 @@ public class WorryController {
                     name = "categories"
                     , value = "카테고리PK List"
             )
-    })*/
+    })
     @GetMapping("/past/filter")
     public ResponseEntity<List<WorryPastResponseDto>> categorizedPastWorry(@RequestParam("userId") Long userId, @RequestParam("categories") List<Long> categoryId) {
         try {
@@ -126,10 +126,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 지난 걱정 - 의미 있는 걱정
-    /*@ApiOperation(value = "걱정 보관함 - 지난 걱정 - 의미있는 걱정", notes = "걱정 보관함에서 실현된 지난 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 지난 걱정 - 의미있는 걱정", notes = "걱정 보관함에서 실현된 지난 걱정 불러오기")
     @ApiImplicitParam(
             name = "userId"
-            , value = "유저PK")*/
+            , value = "유저PK")
     @GetMapping("/past/mean")
     public ResponseEntity<List<WorryPastResponseDto>> meaningfulWorry(@RequestParam("userId") Long userId) {
         try {
@@ -142,10 +142,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 지난 걱정 - 의미 없는 걱정
-    /*@ApiOperation(value = "걱정 보관함 - 지난 걱정 - 의미없는 걱정", notes = "걱정 보관함에서 실현되지 않은 지난 걱정 불러오기")
+    @ApiOperation(value = "걱정 보관함 - 지난 걱정 - 의미없는 걱정", notes = "걱정 보관함에서 실현되지 않은 지난 걱정 불러오기")
     @ApiImplicitParam(
             name = "userId"
-            , value = "유저PK")*/
+            , value = "유저PK")
     @GetMapping("/past/meanless")
     public ResponseEntity<List<WorryPastResponseDto>> meanlessWorry(@RequestParam("userId") Long userId) {
         try {
@@ -158,10 +158,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 걱정 잠금/해제
-    /*@ApiOperation(value = "걱정 보관함 - 걱정 잠금/해제", notes = "걱정을 잠금 or 잠금 해제한다.")
+    @ApiOperation(value = "걱정 보관함 - 걱정 잠금/해제", notes = "걱정을 잠금 or 잠금 해제한다.")
     @ApiImplicitParam(
             name = "worryId"
-            , value = "걱정PK")*/
+            , value = "걱정PK")
     @PatchMapping("/{worryId}")
     public ResponseEntity turnLockState(@PathVariable Long worryId) {
         worryService.turnWorryLockState(worryId);
@@ -169,10 +169,10 @@ public class WorryController {
     }
 
     //걱정 보관함 - 걱정 삭제
-    /*@ApiOperation(value = "걱정 삭제", notes = "걱정 보관함의 걱정 삭제")
+    @ApiOperation(value = "걱정 삭제", notes = "걱정 보관함의 걱정 삭제")
     @ApiImplicitParam(
             name = "worryIds"
-            , value = "걱정PK 리스트")*/
+            , value = "걱정PK 리스트")
     @DeleteMapping
     public ResponseEntity removeWorry(@RequestParam("worryIds") List<Long> worryIds) {
         try {
@@ -184,10 +184,10 @@ public class WorryController {
     }
 
     //후기 작성 채팅방 - 열기
-    /*@ApiOperation(value = "후기 작성 채팅방 오픈", notes = "후기 작성 채팅방 오픈시 필요한 데이터 호출")
+    @ApiOperation(value = "후기 작성 채팅방 오픈", notes = "후기 작성 채팅방 오픈시 필요한 데이터 호출")
     @ApiImplicitParam(
             name = "worryId"
-            , value = "걱정PK")*/
+            , value = "걱정PK")
     @GetMapping("/chat/{worryId}")
     public ResponseEntity<WorryChatResponseDto> openWorryChat(@PathVariable Long worryId) {
         WorryChatResponseDto worryChatResponseDto = worryService.worryChatOpen(worryId);
@@ -195,7 +195,7 @@ public class WorryController {
     }
 
     //후기 작성 채팅방 - 걱정 실현 여부 입력
-    /*@ApiOperation(value = "후기 작성 채팅방 - 실현 여부 입력", notes = "걱정 실현 여부 입력")
+    @ApiOperation(value = "후기 작성 채팅방 - 실현 여부 입력", notes = "걱정 실현 여부 입력")
     @ApiImplicitParams({
             @ApiImplicitParam(
                     name = "userId"
@@ -206,7 +206,7 @@ public class WorryController {
             @ApiImplicitParam(
                     name = "isRealized"
                     , value = "걱정 실현 여부")
-    })*/
+    })
     @PostMapping("/chat/realize")
     public ResponseEntity<WorryCntResponseDto> inputWorryIsRealized(@RequestParam("userId") Long userId, @RequestParam("worryId") Long worryId, @RequestParam("isRealized") boolean isRealized) {
         try {
@@ -219,7 +219,7 @@ public class WorryController {
     }
 
     //후기 작성 채팅방 - 걱정 만료일 수정
-    /*@ApiOperation(value = "후기 작성 채팅방 - 걱정 만료일 수정", notes = "걱정이 끝나지 않았을 때 만료일 수정")
+    @ApiOperation(value = "후기 작성 채팅방 - 걱정 만료일 수정", notes = "걱정이 끝나지 않았을 때 만료일 수정")
     @ApiImplicitParams({
             @ApiImplicitParam(
                     name = "worryId"
@@ -227,7 +227,7 @@ public class WorryController {
             @ApiImplicitParam(
                     name = "expiryDate"
                     , value = "걱정 만료일")
-    })*/
+    })
     @PutMapping("/review/date")
     public ResponseEntity changeWorryExpiryDate(@RequestParam("worryId") Long worryId, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam("expiryDate") LocalDateTime expiryDate) {
         boolean check = worryService.changeWorryExpiryDate(worryId, expiryDate);
@@ -240,10 +240,10 @@ public class WorryController {
     }
 
     //걱정 후기 불러오기
-    /*@ApiOperation(value = "걱정 후기 불러오기", notes = "걱정 후기 데이터를 불러온다")
+    @ApiOperation(value = "걱정 후기 불러오기", notes = "걱정 후기 데이터를 불러온다")
     @ApiImplicitParam(
             name = "worryId"
-            , value = "걱정PK")*/
+            , value = "걱정PK")
     @GetMapping("/review/{worryId}")
     public ResponseEntity<WorryReviewResponseDto> callWorryReview(@PathVariable Long worryId) {
         try {
@@ -257,7 +257,7 @@ public class WorryController {
     }
 
     //걱정 후기 수정 - 실현 여부 수정
-    /*@ApiOperation(value = "걱정 후기 수정 - 실현 여부 수정", notes = "걱정 실현 여부 수정")
+    @ApiOperation(value = "걱정 후기 수정 - 실현 여부 수정", notes = "걱정 실현 여부 수정")
     @ApiImplicitParams({
             @ApiImplicitParam(
                     name = "worryId"
@@ -265,7 +265,7 @@ public class WorryController {
             @ApiImplicitParam(
                     name = "isRealized"
                     , value = "걱정 실현 여부")
-    })*/
+    })
     @PatchMapping("/review/realize")
     public ResponseEntity modifyIsRealized(@RequestParam("worryId") Long worryId, @RequestParam("isRealized") boolean isRealized) {
         try {
@@ -277,7 +277,7 @@ public class WorryController {
     }
 
     //걱정 후기 입력/수정 - 내용
-//    @ApiOperation(value = "걱정 후기 입력/수정 - 내용", notes = "후기 내용 입력 OR 수정")
+    @ApiOperation(value = "걱정 후기 입력/수정 - 내용", notes = "후기 내용 입력 OR 수정")
     @PatchMapping("/review")
     public ResponseEntity modifyWorryReview(@RequestBody WorryReviewModifyRequestDto worryRequestDto) {
         try {
